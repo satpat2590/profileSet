9261cec3f6f43fccac6731e3828529fa81002f87
Sat Patel <sbp223@bitbucket.com>
2023-01-04 17:48:04 -0500

    yaaa baby

---
+++ spotify-dev/spotify-real/spotifyd/spotify.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+alias shuff='spt playback --shuffle'
+alias nextplz='spt playback --next'
+alias likedalbums='spt list --liked -f --limit=50 %a'
+alias listplaylist='spt list --playlists -f --limit=50 %a'
+alias listplaylists2='spt list --playlists --limit 2 -f %a'
+alias visualize='spt --tick-rate=5'
